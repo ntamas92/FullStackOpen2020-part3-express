9142e4fdd589179e2020a3f00e7e8cf07efb971f
Tamas Nagy <nredline@gmail.com>
2020-07-09 17:07:49 +0200

    Initialize heroku

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get('/api/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${phonebookEntries.length} people.</p><p>${new Date()}}</p>`);
 })
 
-const PORT = 3001;
+const PORT = proces.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
