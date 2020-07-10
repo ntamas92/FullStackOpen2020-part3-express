53f6c0d225293b9af3cdb2db056fefad71eef22b
Tamas Nagy <nredline@gmail.com>
2020-07-10 14:20:41 +0200

    Add initial frontend build

---
+++ build/precache-manifest.62e2086b007b5c23e61c1c25a3e774cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb1d465031110967faba2c58631a4543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e71d801ca8162975a796",
+    "url": "/static/css/main.a64249ec.chunk.css"
+  },
+  {
+    "revision": "dbaf8825220e57e56d18",
+    "url": "/static/js/2.ad435a02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ad435a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e71d801ca8162975a796",
+    "url": "/static/js/main.01e8e335.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
